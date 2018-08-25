eb86c12cc06f424bf8d466ac4eda3c43008396d2
Maximilian Gravenstein <grax@Maxs-MacBook.local>
2018-08-24 20:39:52 -0400

    example bitcoin prices for testing

--- file1.py
+++ file1.py
@@ -1,6 +1,15 @@
-import time
+#Steps
+#1. Read the csv file
+#2. Convert the dates to numbers
+#3. Plot (date, number)
+import csv
 
-y=3
-x = y+3
-print(x)
-print('hello')
+with open('bitcoin.csv') as file:
+    count = 0
+    reader = csv.reader(file)
+    
+    for row in reader:
+        print(row)
+        if count > 5:
+            break
+        count +=1
