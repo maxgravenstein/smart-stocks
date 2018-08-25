d2928eae986aecc2b7a2fc41482e1bfd612b1980
Maximilian Gravenstein <grax@Maxs-MacBook.local>
2018-08-25 16:26:30 -0400

    proof of concept

--- file1.py
+++ file1.py
@@ -2,12 +2,13 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-dates = [2010-7-19, 2010-7-20, 2010-7-21, 2010-7-22, 2010-7-23]
+dates = ['2010-7-19', '2010-7-20', '2010-7-21', '2010-7-22', '2010-7-23']
 x = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dates]
 y = range(len(dates))
-#plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-#plt.gca().xaxis.set_major_locator(mdates.DayLocator())
 #plt.plot(x,y)
-#plt.gcf().autofmt_xdate()
 
-print(x,y)
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.plot(x,y)
+plt.gcf().autofmt_xdate()
+plt.show()
+#print(x,y)
