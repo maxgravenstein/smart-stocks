de4ec1a3ca11859cca287fa8ad2313aa3d39fb1e
Maximilian Gravenstein <grax@Maxs-MacBook.local>
2018-08-25 12:32:42 -0400

    branch

--- file1.py
+++ file1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 dates = [2010-7-19, 2010-7-20, 2010-7-21, 2010-7-22, 2010-7-23]
-x = dt.datetime.strptime(row[0][0:10], '%Y-%m-%d').date()
+x = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dates]
 y = range(len(dates))
 #plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 #plt.gca().xaxis.set_major_locator(mdates.DayLocator())
