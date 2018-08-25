ffd09bf3abffd7a846a79d4a9e7dd24080a5dd59
Maximilian Gravenstein <grax@Maxs-MacBook.local>
2018-08-25 12:30:44 -0400

    BRANCH

--- file1.py
+++ file1.py
@@ -1,28 +1,13 @@
-#Steps
-#1. Read the csv file 
-#2. Convert the dates to numbers
-#3. Plot (date, number)
-import csv
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-with open('bitcoin.csv') as file:
-    count = 0
-    reader = csv.reader(file)
-    next(reader, None) #skip the headers
-    
-    for row in reader:
-        x = dt.datetime.strptime(row[0][0:10], '%Y-%m-%d').date()
-        y = float(row[1])
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-        plt.plot(x,y)
-        plt.gcf().autofmt_xdate()
-        
-        print(x,y)
-        if count > 5:
-            break
-        count +=1
+dates = [2010-7-19, 2010-7-20, 2010-7-21, 2010-7-22, 2010-7-23]
+x = dt.datetime.strptime(row[0][0:10], '%Y-%m-%d').date()
+y = range(len(dates))
+#plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+#plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+#plt.plot(x,y)
+#plt.gcf().autofmt_xdate()
 
-#datetime format: %Y-%m-%d
+print(x,y)
